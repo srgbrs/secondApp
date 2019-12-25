a87e08e57f19256f390b1faa4d0687332ed5ecd0
srgbrs <borisov11092000@gmail.com>
2019-12-26 01:21:15 +0200

    view engine connected

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const exphbs = require('express-handlebars')
 const PORT = process.env.PORT || 3000
 
 const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views','views')
 
 async function start() {
     try {
