026666047774ffd2096df4a7c90a922777d1203b
srgbrs <borisov11092000@gmail.com>
2019-12-26 02:29:14 +0200

    Menu created

--- routes/list.js
+++ routes/list.js
@@ -3,7 +3,19 @@ const{Router} = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Список студентов',
+        isIndex: true
+
+    })
+})
+
+router.get('/create', (req,res)=>{
+    res.render('create', {
+        title: 'Добавить студента',
+        isCreate: true
+    })
+
 })
 
 module.exports = router // экспорт команды
