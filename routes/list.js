0d2c0e1d6841b8ba6bfc3bd01aaf80d181d7124e
srgbrs <borisov11092000@gmail.com>
2019-12-26 17:03:10 +0200

    Add student page

--- routes/list.js
+++ routes/list.js
@@ -1,11 +1,14 @@
 const{Router} = require('express')
+const List = require('../models/List')
 
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const list = await List.find({})
     res.render('index', {
         title: 'Список студентов',
-        isIndex: true
+        isIndex: true,
+        list
 
     })
 })
